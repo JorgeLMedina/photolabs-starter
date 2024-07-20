a0bfbfa9e4f11f8764ff8330716f599a1ffdd155
Jorge Medina <jlmedinamacias@gmail.com>
2024-07-19 17:52:53 -0700

    Adjust first component ('PhotoListItem.jsx') so it can take props data correctly from App.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,17 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  return (
+    <article>
+      <img src={props.imageSource} />
+      <div className="user-name">
+        <img src={props.profile} />
+        <h3>{props.username}</h3>
+        <h3>{`${props.location.city}, ${props.location.country}`}</h3>
+      </div>
+    </article>
+  );
 };
 
 export default PhotoListItem;
