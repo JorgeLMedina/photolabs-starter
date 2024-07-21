d8b89ca1255163310139cec43b2bbdc3b44b2754
Jorge Medina <jlmedinamacias@gmail.com>
2024-07-20 19:59:37 -0700

    Move main components(TopNavigation and PhotoList) to HomeRoute file in routes folder instead of components folder, adjust App.jsx imports accordingly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // frontend/src/App.jsx
 import React from 'react';
 import './App.scss';
-import HomeRoute from 'components/HomeRoute';
+import HomeRoute from 'routes/HomeRoute';
 
 
 const App = () => {
