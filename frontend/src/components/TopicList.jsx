189428caa03ea36ce006c6a38596dd806063b268
Jorge Medina <jlmedinamacias@gmail.com>
2024-07-20 20:07:09 -0700

    Display mock data imported from 'mocks' folder, remove default props.

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,27 +2,10 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "mocks/topics";
 
 const TopicList = () => {
-  const topicListArr = sampleDataForTopicList.map(item => <TopicListItem key={item.id} topic={item} />);
+  const topicListArr = topics.map(item => <TopicListItem key={item.id} topic={item} />);
 
   return (
     <div className="top-nav-bar__topic-list">
